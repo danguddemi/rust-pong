5d074bbdccfa1dc1aa26d98c366e020caeea293e
Daniel Guddemi <danguddemi@gmail.com>
2021-02-06 17:26:50 -0500

    Adding ball movement and paddle collision detection

--- src/pong.rs
+++ src/pong.rs
@@ -18,6 +18,7 @@ pub const BALL_VELOCITY_X: f32 = 75.0;
 pub const BALL_VELOCITY_Y: f32 = 50.0;
 pub const BALL_RADIUS: f32 = 2.0;
 
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum Side {
     Left,
     Right,
