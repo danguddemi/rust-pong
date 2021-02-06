eda2994c8e5f222d3e52bb0d4587db6554688008
Daniel Guddemi <danguddemi@gmail.com>
2021-02-06 16:42:54 -0500

    Adding paddle transformation system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod pong;
+mod systems;
+
 use amethyst::{
     prelude::*,
     renderer::{
@@ -7,9 +10,11 @@ use amethyst::{
     },
     utils::application_root_dir,
     core::transform::TransformBundle,
+    input::{
+        InputBundle,
+        StringBindings,
+    },
 };
-
-mod pong;
 
 use crate::pong::Pong;
 
@@ -17,8 +22,15 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
@@ -26,9 +38,6 @@ fn main() -> amethyst::Result<()> {
                         .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
                 .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(
-            TransformBundle::new()
         )?;
     let assest_dir = app_root.join("assets");
     let mut game = Application::new(assest_dir, Pong, game_data)?;
