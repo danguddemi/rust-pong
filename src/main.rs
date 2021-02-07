1195a98796a7eadf311b242d247921b5cccf5f2a
Daniel Guddemi <danguddemi@gmail.com>
2021-02-07 13:46:18 -0500

    Adding point system for recording wins

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ fn main() -> amethyst::Result<()> {
         .with_bundle(input_bundle)?
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"])
         .with(
             systems::BounceSystem,
             "collision_system",
